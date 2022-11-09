0d853905ffa70bbd9b1343878aa79f3c0fe22769
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-09 18:19:21 +0000

    get feed parser initially working

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+)
+
+func main() {
+  fp := gofeed.NewParser()
+  feed, _ := fp.ParseURL("https://www.youtube.com/feeds/videos.xml?channel_id=UCc0YbtMkRdhcqwhu3Oad-lw")
+  fmt.Println(feed.Title)
+}
