4360ec7ddb1fa04a08c3d53ab08242d42762c270
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-09 20:14:53 +0000

    make item

--- pkg/feed/feed.go
+++ pkg/feed/feed.go
@@ -13,6 +13,21 @@ type Attachment struct {
 	Type   *string
 }
 
+type Item struct {
+	Title       *string
+	Description *string
+	Content     *string
+	Link        *string
+	Updated     *string
+	Published   *string
+	Author      *string
+	Authors     []*string
+	GUID        *string
+	Image       *Image
+	Categories  []*string
+	Attachments []*Attachment
+}
+
 type Feed struct {
 	Title       *string
 	Description *string
@@ -20,6 +35,7 @@ type Feed struct {
 	FeedLink    *string
 	Updated     *string
 	Links       []*string
+	Items       []*Item
 	Published   *string
 	Author      *string
 	Authors     []*string
@@ -30,22 +46,7 @@ type Feed struct {
 	Categories  []*string
 }
 
-type Item struct {
-	Title       *string
-	Description *string
-	Content     *string
-	Link        *string
-	Updated     *string
-	Published   *string
-	Author      string
-	Authors     []*string
-	GUID        *string
-	Image       *Image
-	Categories  []*string
-	Files       []*Attachment
-}
-
-func cloneFeed(theirs *gofeed.Feed) *Feed {
+func makeFeed(theirs *gofeed.Feed) *Feed {
 	ourImg := new(Image)
 	ourImg.Title = &theirs.Image.Title
 	ourImg.URL = &theirs.Image.URL
@@ -73,10 +74,50 @@ func cloneFeed(theirs *gofeed.Feed) *Feed {
 	return ours
 }
 
+func makeItem(theirs *gofeed.Item) *Item {
+	ourImg := new(Image)
+	ourImg.Title = &theirs.Image.Title
+	ourImg.URL = &theirs.Image.URL
+
+	ours := new(Item)
+	ours.Title = &theirs.Title
+	ours.Description = &theirs.Description
+	ours.Content = &theirs.Content
+	ours.Link = &theirs.Link
+	ours.Updated = &theirs.Updated
+	ours.Published = &theirs.Published
+	ours.Author = &theirs.Author.Name
+	ours.GUID = &theirs.GUID
+	ours.Image = ourImg
+	for i := 0; i < len(theirs.Authors); i++ {
+		ours.Authors[i] = &theirs.Authors[i].Name
+	}
+	for i := 0; i < len(theirs.Categories); i++ {
+		ours.Categories[i] = &theirs.Categories[i]
+	}
+	for i := 0; i < len(theirs.Enclosures); i++ {
+		ours.Attachments[i].URL = &theirs.Enclosures[i].URL
+		ours.Attachments[i].Length = &theirs.Enclosures[i].Length
+		ours.Attachments[i].Type = &theirs.Enclosures[i].Type
+	}
+
+	return ours
+}
+
+func makeItems(theirs []*gofeed.Item) []*Item {
+	var result []*Item
+	for i := 0; i < len(theirs); i++ {
+		result[i] = makeItem(theirs[i])
+	}
+	return result
+}
+
 func Get(url *string) (*Feed, error) {
 	fp := gofeed.NewParser()
 	theirFeed, err := fp.ParseURL(*url)
-	ourFeed := cloneFeed(theirFeed)
+	ourFeed := makeFeed(theirFeed)
+	ourItems := makeItems(theirFeed.Items)
+	ourFeed.Items = ourItems
 
 	return ourFeed, err
 }
