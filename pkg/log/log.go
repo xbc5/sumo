1d5703bc0b1dfcf2ebd558e2982caa6bef291dff
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-12 04:05:11 +0000

    remove bools from logger

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -11,47 +11,37 @@ func init() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 }
 
-func QueryError(msg string, err error) bool {
+func QueryError(msg string, err error) {
 	if err != nil {
 		log.Logger.Err(err).Str("kind", "query").Msg(msg)
-		return true
 	}
-	return false
 }
 
-func FeedQueryErr(msg string, url string, err error) bool {
+func FeedQueryErr(msg string, url string, err error) {
 	if err != nil {
 		log.Logger.Err(err).Str("kind", "query").Str("table", "Feed").Str("url", url).Msg(msg)
-		return true
 	}
-	return false
 }
 
-func FeedGetErr(url string, err error) bool {
+func FeedGetErr(url string, err error) {
 	if err != nil {
 		log.Logger.Err(err).Str("kind", "fetch").Str("url", url).Msg("Error fetching feed")
-		return true
 	}
-	return false
 }
 
-func SchemaFatal(msg string, query string, err error) bool {
+func SchemaFatal(msg string, query string, err error) {
 	if err != nil {
 		log.Logger.Fatal().AnErr("error", err).Str("kind", "schema").Str("query", query).Msg(msg)
-		return true
 	}
-	return false
 }
 
-func SchemaInfo(msg string, tableName string) bool {
+func SchemaInfo(msg string, tableName string) {
 	log.Logger.Info().Str("kind", "schema").Str("table", tableName).Msg(msg)
-	return true
+
 }
 
-func DbConnErr(msg string, err error) bool {
+func DbConnErr(msg string, err error) {
 	if err != nil {
 		log.Logger.Err(err).Str("kind", "connection").Msg(msg)
-		return true
 	}
-	return false
 }
