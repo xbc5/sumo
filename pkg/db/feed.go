d8db5c309c11dc6357048e002bbfd89ced6d7d18
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-12 20:55:44 +0000

    write initial UpsertArticle

--- pkg/db/feed.go
+++ pkg/db/feed.go
@@ -20,6 +20,45 @@ func (this *Feed) InsertUrl(url string) (sql.Result, error) {
 	return result, err
 }
 
+const upsertArticleQuery = `
+IF EXISTS (SELECT * FROM Article WITH (url) WHERE url = @url)
+  BEGIN
+    UPDATE table SET
+      title = @title,
+      description = @description,
+      content = @content,
+      udated = @updated,
+      published = @published,
+      banner = @banner
+    WHERE url = @url
+  END
+ELSE
+  BEGIN
+    INSERT INTO TABLE  ( url,  title,  description,  content,  updated,  published,  banner)
+                VALUES (@url, @title, @description, @content, @updated, @published, @banner)
+  END
+`
+
+func (this *Feed) UpsertArticle(art *feed.Item) error {
+	tx, txErr := this.Db.Begin()
+	log.FeedQueryErr("Cannot begin TX in UpsertArticle()", nil, txErr)
+	stmt, stmtErr := tx.Prepare(upsertArticleQuery)
+	log.FeedQueryErr("Cannot prepare statement in UpsertArticle()", nil, stmtErr)
+	_, execErr := stmt.Exec(
+		sql.Named("url", art.Link),
+		sql.Named("title", art.Title),
+		sql.Named("description", art.Description),
+		sql.Named("content", art.Content),
+		sql.Named("updated", art.Updated),
+		sql.Named("published", art.Published),
+		sql.Named("banner", art.Banner.URL),
+	)
+	stmt.Close() // FIXME handle error
+	log.FeedQueryErr("Cannot execute statement in UpsertArticle()", nil, execErr)
+	tx.Commit()
+	return execErr
+}
+
 const updateFeedQuery = `UPDATE Feed
 SET title = ?, description = ?, language = ?, logo = ?
 WHERE url = ?`
