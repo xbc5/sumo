8199d69f0edac91de15edad6d994f2d1a3774f90
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-22 13:12:19 +0000

    set some functions

--- api/common.go
+++ api/common.go
@@ -3,16 +3,30 @@ package api
 import (
 	"github.com/rs/zerolog"
 	db "github.com/xbc5/sumo/lib/database"
+	"github.com/xbc5/sumo/lib/database/model"
+	"github.com/xbc5/sumo/lib/feed"
+	"github.com/xbc5/sumo/lib/log"
 	"gorm.io/gorm"
 )
 
 type API struct {
-	db      *gorm.DB
-	DSN     string
-	OnDBErr func(err error) *zerolog.Event
+	db          *gorm.DB
+	DSN         string
+	OnDBErr     func(err error) *zerolog.Event
+	GetFeedUrls func(db *gorm.DB) ([]string, error)
+	GetPatterns func(db *gorm.DB) ([]model.Pattern, error)
+	TagFeed     func(feed model.Feed, patterns []model.Pattern) (model.Feed, error)
+	SaveFeed    func(db *gorm.DB, feed model.Feed) error
 }
 
 func (this *API) New() *API {
+	this.DSN = "file"
+	this.OnDBErr = log.DbErr
+	this.GetFeedUrls = db.GetFeedURLs
+	this.GetPatterns = db.GetAllPatterns
+	this.TagFeed = feed.Tag
+	this.SaveFeed = db.UpdateFeed
+
 	d, err := db.Open(this.DSN, nil)
 	if err != nil {
 		this.OnDBErr(err).Msg("Cannot connect to the database")
