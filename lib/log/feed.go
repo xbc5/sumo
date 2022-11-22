2d475c63a31de9a72d2af6c425078eb32ba1628f
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-22 15:49:40 +0000

    add Tag error logger

--- lib/log/feed.go
+++ lib/log/feed.go
@@ -7,3 +7,7 @@ import (
 func FetchErr(url string, err error) {
 	log.Logger.Err(err).Str("kind", "fetch").Str("url", url).Msg("Cannot fetch feed")
 }
+
+func TagErr(url string, err error) {
+	log.Logger.Err(err).Str("kind", "tag").Str("url", url).Msg("Cannot tag feed")
+}
