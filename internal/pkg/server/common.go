092b8d30f9869042c796bd450125ad12c5bcbda3
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-30 13:13:10 +0000

    create a test server

--- internal/pkg/server/common.go
+++ internal/pkg/server/common.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 
 	"github.com/xbc5/sumo/internal/pkg/config"
 )
@@ -12,15 +13,22 @@ type Server struct {
 	Config      config.Config
 }
 
-func (this Server) Start() {
+func (this Server) createHandler() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", this.handleRoot)
 	mux.HandleFunc("/ws", this.handleWs)
+	return mux
+}
 
-	err := http.ListenAndServe(this.Config.Server.Address, mux)
+func (this Server) Start() {
+	err := http.ListenAndServe(this.Config.Server.Address, this.createHandler())
 	if err != nil {
 		fmt.Errorf("Cannot start server: %s", err) // TODO log error
 	}
+}
+
+func (this Server) StartTest() *httptest.Server {
+	return httptest.NewServer(this.createHandler())
 }
 
 func (this *Server) New() *Server {
