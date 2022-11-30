756a1124b61df88a92daf6760094215077d20a58
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-30 16:43:09 +0000

    test that it's 200 OK

--- internal/pkg/server/common_test.go
+++ internal/pkg/server/common_test.go
@@ -12,13 +12,21 @@ func serv() server.ServerBuilder {
 }
 
 var _ = Describe("Common", func() {
-	Context("after starting the server", func() {
+	Context("after starting the server and connecting a client to it", func() {
 		It("should not error", func() {
 			s := serv().Build().StartTest()
 			c := s.Client()
 			defer s.Close()
 			_, err := c.Get(s.URL)
 			Expect(err).To(BeNil())
+		})
+
+		It("should return 200 OK", func() {
+			s := serv().Build().StartTest()
+			c := s.Client()
+			defer s.Close()
+			res, _ := c.Get(s.URL)
+			Expect(res.StatusCode).To(Equal(200))
 		})
 	})
 })
