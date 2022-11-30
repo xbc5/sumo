80a724427ec7699b0284a00bc226460a82a7aaad
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-30 10:28:10 +0000

    tidy

--- internal/pkg/server/common.go
+++ internal/pkg/server/common.go
@@ -13,6 +13,7 @@ func (this Server) Start() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", this.handleRoot)
 	mux.HandleFunc("/ws", this.handleWs)
+
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Errorf("Cannot start server: %s", err)
