4aceab9f65ed3dadd987ff9ffc2d2cc77ce55772
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-30 10:00:47 +0000

    do basic WS implementation

--- internal/pkg/server/common.go
+++ internal/pkg/server/common.go
@@ -10,6 +10,7 @@ type Server struct{}
 func (this Server) Start() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", this.handleRoot)
+	mux.HandleFunc("/ws", this.handleWs)
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Errorf("Cannot start server: %s", err)
--- internal/pkg/server/routes.go
+++ internal/pkg/server/routes.go
@@ -3,8 +3,38 @@ package server
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func (this Server) handleRoot(res http.ResponseWriter, req *http.Request) {
 	fmt.Printf("root handled")
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func checkOrigin(req *http.Request) bool {
+	return true
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Printf("WS read error") // TODO log error
+		}
+		fmt.Printf(string(p))
+	}
+}
+
+func (this Server) handleWs(res http.ResponseWriter, req *http.Request) {
+	conn, err := upgrader.Upgrade(res, req, nil)
+	upgrader.CheckOrigin = checkOrigin
+	if err != nil {
+		fmt.Printf("WebSocket connection error") // TODO: log error
+	}
+	reader(conn)
 }
