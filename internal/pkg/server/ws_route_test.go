cdca6c1d29eb6b040ee11411613c0a8dd1a99747
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-30 10:02:31 +0000

    create ws route test modules

---
+++ internal/pkg/server/ws_route_test.go
@@ -0,0 +1,12 @@
+package server_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/xbc5/sumo/internal/pkg/server"
+)
+
+var _ = Describe("WsRoute", func() {
+
+})
