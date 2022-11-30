759c927f8b8acd3bfe64311dbdae1bf847614fa4
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-30 17:01:05 +0000

    create / tests; make all pass except body value

---
+++ internal/pkg/server/home_route_test.go
@@ -0,0 +1,53 @@
+package server_test
+
+import (
+	"io"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("/", func() {
+	Context("when connecting to /", func() {
+		It("should not error", func() {
+			s := serv().Build().StartTest()
+			c := s.Client()
+			defer s.Close()
+
+			res, err := c.Get(s.URL + "/")
+			if res != nil {
+				defer res.Body.Close()
+			}
+
+			Expect(err).To(BeNil())
+		})
+
+		It("should return 200 OK", func() {
+			s := serv().Build().StartTest()
+			c := s.Client()
+			defer s.Close()
+
+			res, _ := c.Get(s.URL + "/")
+			if res != nil {
+				defer res.Body.Close()
+			}
+
+			Expect(res.StatusCode).To(Equal(200))
+		})
+
+		It("should return an expected result", func() {
+			s := serv().Build().StartTest()
+			c := s.Client()
+			defer s.Close()
+
+			res, _ := c.Get(s.URL + "/")
+			if res != nil {
+				defer res.Body.Close()
+			}
+			body, err := io.ReadAll(res.Body)
+
+			Expect(err).To(BeNil())
+			Expect(string(body)).To(Equal("test"))
+		})
+	})
+})
