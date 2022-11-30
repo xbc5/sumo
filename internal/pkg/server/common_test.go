2acb56bea18ad7a946e44ff6262f0b52367bb166
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-30 16:40:04 +0000

    refactor, use bult-in client

--- internal/pkg/server/common_test.go
+++ internal/pkg/server/common_test.go
@@ -1,8 +1,6 @@
 package server_test
 
 import (
-	"net/http"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/xbc5/sumo/internal/pkg/server"
@@ -17,11 +15,10 @@ var _ = Describe("Common", func() {
 	Context("when starting the server", func() {
 		It("should start", func() {
 			s := serv().Build().StartTest()
+			c := s.Client()
 			defer s.Close()
-			req, _ := http.NewRequest("GET", s.URL, nil)
-			res, resErr := http.DefaultClient.Do(req)
-			Expect(resErr).To(BeNil())
-			defer res.Body.Close()
+			_, err := c.Get(s.URL)
+			Expect(err).To(BeNil())
 		})
 	})
 })
