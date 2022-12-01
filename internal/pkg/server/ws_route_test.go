a6c204b8eecd1fd4ee0232980c43faf0f0e48c71
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-12-01 14:30:34 +0000

    test that sending a message does not error

--- internal/pkg/server/ws_route_test.go
+++ internal/pkg/server/ws_route_test.go
@@ -17,6 +17,12 @@ func wsUrl(u string) string {
 	return fmt.Sprintf("ws://%s/ws", _url.Host)
 }
 
+const json string = `
+{
+  "test": "test value"
+}
+`
+
 var _ = Describe("/ws route", func() {
 	Context("when connecting", func() {
 		It("should upgrade connection", func() {
@@ -29,6 +35,20 @@ var _ = Describe("/ws route", func() {
 
 			Expect(err).To(BeNil())
 			Expect(res.StatusCode).To(Equal(101)) // upgrading to WS
+		})
+	})
+
+	Context("when sending a message", func() {
+		It("should not error", func() {
+			s := serv().Build().StartTest()
+			defer s.Close()
+
+			fmt.Printf(s.URL)
+			conn, _, _ := websocket.DefaultDialer.Dial(wsUrl(s.URL), nil)
+			defer conn.Close()
+			err := conn.WriteMessage(websocket.TextMessage, []byte(json))
+
+			Expect(err).To(BeNil())
 		})
 	})
 })
