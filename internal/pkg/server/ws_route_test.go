7daf037372964f80214050e8c94e29601d15adef
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-12-01 14:21:46 +0000

    do basic connection test

--- internal/pkg/server/ws_route_test.go
+++ internal/pkg/server/ws_route_test.go
@@ -1,67 +1,34 @@
 package server_test
 
 import (
-	"io"
+	"fmt"
+	"net/url"
 
-	"github.com/bradleyjkemp/cupaloy"
+	"github.com/gorilla/websocket"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("/", func() {
-	Context("when connecting to /", func() {
-		It("should not error", func() {
+func wsUrl(u string) string {
+	_url, err := url.ParseRequestURI(u)
+	if err != nil {
+		panic(fmt.Sprintf("Cannot parse URL: %q", u))
+	}
+	return fmt.Sprintf("ws://%s/ws", _url.Host)
+}
+
+var _ = Describe("/ws route", func() {
+	Context("when connecting", func() {
+		It("should upgrade connection", func() {
 			s := serv().Build().StartTest()
-			c := s.Client()
 			defer s.Close()
 
-			res, err := c.Get(s.URL + "/")
-			if res != nil {
-				defer res.Body.Close()
-			}
+			fmt.Printf(s.URL)
+			conn, res, err := websocket.DefaultDialer.Dial(wsUrl(s.URL), nil)
+			defer conn.Close()
 
 			Expect(err).To(BeNil())
-		})
-
-		It("should return 200 OK", func() {
-			s := serv().Build().StartTest()
-			c := s.Client()
-			defer s.Close()
-
-			res, _ := c.Get(s.URL + "/")
-			if res != nil {
-				defer res.Body.Close()
-			}
-
-			Expect(res.StatusCode).To(Equal(200))
-		})
-
-		It("should return text/html Content-Type", func() {
-			s := serv().Build().StartTest()
-			c := s.Client()
-			defer s.Close()
-
-			res, _ := c.Get(s.URL + "/")
-			if res != nil {
-				defer res.Body.Close()
-			}
-
-			Expect(res.Header.Get("Content-Type")).To(Equal("text/html; charset=utf-8"))
-		})
-
-		It("should return an expected result", func() {
-			s := serv().Build().StartTest()
-			c := s.Client()
-			defer s.Close()
-
-			res, _ := c.Get(s.URL + "/")
-			if res != nil {
-				defer res.Body.Close()
-			}
-			body, err := io.ReadAll(res.Body)
-
-			Expect(err).To(BeNil())
-			cupaloy.SnapshotT(GinkgoT(), body)
+			Expect(res.StatusCode).To(Equal(101)) // upgrading to WS
 		})
 	})
 })
